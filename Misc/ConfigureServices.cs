e74335d05752491764e4aaae1cc0e6737c16f240
filebin <alex.mousse9@gmail.com>
2024-04-10 00:16:37 +0200

    added authorization support

--- Misc/ConfigureServices.cs
+++ Misc/ConfigureServices.cs
@@ -1,8 +1,11 @@
 ﻿namespace Shared.Misc;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Shared.Misc.Middleware;
 
 public static class ConfigureServices {
@@ -22,19 +25,97 @@ public static class ConfigureServices {
                     ext.Add("instance", $"{ctx.HttpContext.Request.Method} {ctx.HttpContext.Request.Path}");
                 }
             });
-        return services;   
+        return services;
     }
 
     public static void UseUtils(this WebApplication app) {
         app.UseUtils(app.Environment);
     }
 
-        public static void UseUtils(this IApplicationBuilder app, IHostEnvironment env) {
+    public static void UseUtils(this IApplicationBuilder app, IHostEnvironment env) {
         if (env.IsDevelopment()) {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
         app.UseExceptionHandler();
+    }
+
+    public static IServiceCollection AddIdentityServer(this IServiceCollection services, IConfiguration config) {
+        var isconf = config.GetSection("IdentityServer");
+
+        var title = config["SwaggerTitle"] ?? "Protected API";
+
+        var apiName = isconf.GetOrThrow("ApiName");
+        var authority = isconf.GetOrThrow("Authority");
+        var apiScope = isconf.GetSection("ApiScope");
+
+        services.AddAuthentication(options => {
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => {
+                options.Authority = authority;
+                options.RequireHttpsMetadata = false;
+                options.Audience = apiName;
+            });
+
+        services.AddAuthorization(options => {
+            options.AddPolicy("IdentityScope", policy => {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim("scope", apiScope.GetOrThrow("Name"));
+            });
+        });
+
+        services.AddSwaggerGen(options => {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = title, Version = "v1" });
+
+            options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows {
+                    AuthorizationCode = new OpenApiOAuthFlow {
+                        AuthorizationUrl = new Uri($"{authority}/connect/authorize"),
+                        TokenUrl = new Uri($"{authority}/connect/token"),
+                        Scopes = new Dictionary<string, string> {
+                            { apiScope.GetOrThrow("Name"), apiScope.GetOrThrow("Desc") }
+                        }
+                    }
+                }
+            });
+
+            options.OperationFilter<AuthorizeCheckOperationFilter>();
+        });
+
+        return services;
+    }
+
+    public static void UseIdentityServer(this WebApplication app) {
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+
+        var title = app.Configuration["SwaggerTitle"] ?? "Protected API";
+
+        var isconf = app.Configuration.GetSection("IdentityServer:OAuth");
+
+        var clientId = isconf.GetOrThrow("ClientId");
+        var clientSecret = isconf.GetOrThrow("ClientSecret");
+        var appName = isconf.GetOrThrow("AppName");
+
+        if (!app.Environment.IsProduction()) {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", $"{title} V1");
+
+                options.OAuthClientId(clientId);
+                options.OAuthClientSecret(clientSecret);
+                options.OAuthAppName(appName);
+                options.OAuthUsePkce();
+            });
+        }
     }
 }
