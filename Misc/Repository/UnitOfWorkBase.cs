4a768e4a4f977f8a87d0052b79f2a8f130c81f4b
filebin <alex.mousse9@gmail.com>
2024-04-23 14:36:04 +0300

    added revert changes to unit of work

---
+++ Misc/Repository/UnitOfWorkBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using InnoClinic.Shared.Domain.Abstractions;
+
+namespace InnoClinic.Shared.Misc;
+
+public abstract class UnitOfWorkBase : IUnitOfWork {
+    public abstract DbContext GetDbContext();
+
+    public void RevertChanges() {
+        var changedEntries = GetDbContext().ChangeTracker.Entries()
+            .Where(x => x.State != EntityState.Unchanged).ToList();
+
+        foreach (var entry in changedEntries) {
+            switch (entry.State) {
+                case EntityState.Modified:
+                    entry.CurrentValues.SetValues(entry.OriginalValues);
+                    entry.State = EntityState.Unchanged;
+                    break;
+                case EntityState.Added:
+                    entry.State = EntityState.Detached;
+                    break;
+                case EntityState.Deleted:
+                    entry.State = EntityState.Unchanged;
+                    break;
+            }
+        }
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) {
+        return GetDbContext().SaveChangesAsync(cancellationToken);
+    }
+}
